3ea8066412334c8b1d66d7b31ba696dc7c0b29d1
Simba Pfaira <spfaira@yahoo.com>
2021-10-12 01:23:22 +0200

    Update

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const allBookings = require('./bookings.json');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 const app = express();
 var moment = require('moment');
@@ -21,6 +31,115 @@ app.get('/', function (request, response) {
 // TODO add your routes and helper functions here
 app.get('/bookings', function (req, res) {
   res.json(allBookings);
+});
+
+app.get('/customers', async (req, res) => {
+  const result = await pool.query('SELECT * FROM customers ORDER BY name');
+  res.json(result.rows);
+});
+
+app.get('/customers/:customerId', function (req, res) {
+  const { customerId } = req.params;
+
+  pool
+    .query('SELECT * FROM customers WHERE id=$1', [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get('/customers/:customerId/bookings', function (req, res) {
+  const { customerId } = req.params;
+
+  pool
+    .query(
+      'SELECT c.name, h.name, b.checkin_date, b.nights FROM bookings b INNER JOIN customers c ON b.customer_id = c.id INNER JOIN hotels h ON b.hotel_id = h.id WHERE c.id = $1;',
+      [customerId]
+    )
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.put('/customers/:customerId', function (req, res) {
+  const customerId = req.params.customerId;
+  const newEmail = req.body.email;
+
+  if (newEmail) {
+    pool
+      .query('UPDATE customers SET email=$1 WHERE id=$2', [
+        newEmail,
+        customerId,
+      ])
+      .then(() => res.send(`Customer ${customerId} updated!`))
+      .catch((e) => console.error(e));
+  } else {
+    res.status(400).send(`No email provided!`);
+  }
+});
+
+app.delete('/customers/:customerId', function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query('DELETE FROM bookings WHERE customer_id=$1', [customerId])
+    .then(() => {
+      pool
+        .query('DELETE FROM customers WHERE id=$1', [customerId])
+        .then(() => res.send(`Customer ${customerId} deleted!`))
+        .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
+});
+
+app.get('/hotels', function (req, res) {
+  const hotelNameQuery = req.query.name;
+  let query = `SELECT * FROM hotels ORDER BY name`;
+
+  if (hotelNameQuery) {
+    query = `SELECT * FROM hotels WHERE name ILIKE '%${hotelNameQuery}%' ORDER BY name`;
+  }
+
+  pool
+    .query(query)
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get('/hotels/:hotelId', function (req, res) {
+  const { hotelId } = req.params;
+
+  pool
+    .query('SELECT * FROM hotels WHERE id=$1', [hotelId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.post('/hotels', function (req, res) {
+  const newHotelName = req.body.name;
+  const newHotelRooms = req.body.rooms;
+  const newHotelPostcode = req.body.postcode;
+
+  if (!Number.isInteger(newHotelRooms) || newHotelRooms <= 0) {
+    return res
+      .status(400)
+      .send('The number of rooms should be a positive integer.');
+  }
+
+  pool
+    .query('SELECT * FROM hotels WHERE name=$1', [newHotelName])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send('An hotel with the same name already exists!');
+      } else {
+        const query =
+          'INSERT INTO hotels (name, rooms, postcode) VALUES ($1, $2, $3)';
+        pool
+          .query(query, [newHotelName, newHotelRooms, newHotelPostcode])
+          .then(() => res.send('Hotel created!'))
+          .catch((e) => console.error(e));
+      }
+    });
 });
 
 app.get('/bookings/search', function (req, res) {
@@ -124,7 +243,7 @@ app.delete('/bookings/:bookingId', function (req, res) {
     res.status(404).send(`Booking with ID: "${bookingId}" was not found`);
   }
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
